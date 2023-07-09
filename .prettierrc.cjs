8de76688955aa582a843a82e79a56e03e8587a16
ryu <114303361+ryuapp@users.noreply.github.com>
2023-07-09 12:25:48 +0900

    ➕ Add prettier-plugin-tailwindcss

---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro"), require.resolve("prettier-plugin-tailwindcss")],
+  overrides: [
+    {
+      files: [".*", "*.json", "*.md", "*.toml", "*.yml"],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  printWidth: 120,
+  semi: false,
+  trailingComma: "all",
+  useTabs: false,
+}
