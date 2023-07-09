f32b36f85b2fd91b64d816febb7dc45d22e32bf9
ryu <114303361+ryuapp@users.noreply.github.com>
2023-07-09 12:40:40 +0900

    ➖ Remove dead dependency

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test"
-
-// example.spec.ts
-test("Example Visual Regression Testing", async ({ page }) => {
-  await page.goto("http://127.0.0.1:3000/")
-  await expect(page).toHaveScreenshot()
-})
