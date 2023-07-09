d12b29c730fc6fc891155225b499178895f870f4
ryu <114303361+ryuapp@users.noreply.github.com>
2023-07-09 11:05:09 +0900

    🚩 Change to hybrid design
    Hybrid design using black and white

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        brand: "#39b54a",
+        github: "#181717",
+      },
       fontFamily: {
         sans: "Poppins",
       },
