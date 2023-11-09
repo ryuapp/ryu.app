c13f7e5da81ac32100616c3f7d1137d6d75c1d5b
ryu <114303361+ryuapp@users.noreply.github.com>
2023-11-09 21:02:01 +0900

    📦️ Add Astro integration for swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config"
 import cloudflare from "@astrojs/cloudflare"
 import tailwind from "@astrojs/tailwind"
-
 import react from "@astrojs/react"
+import swup from "@swup/astro"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ryu.app",
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), swup({ theme: false })],
   build: {
     format: "file",
   },
