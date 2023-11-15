aca17da8a590ed0b17fb5d9f67b92ecf44ce42af
ryu <114303361+ryuapp@users.noreply.github.com>
2023-11-15 23:59:07 +0900

    ✨ Add ViewTransitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,13 @@ import { defineConfig } from "astro/config"
 import cloudflare from "@astrojs/cloudflare"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
-import swup from "@swup/astro"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ryu.app",
   output: "server",
   adapter: cloudflare(),
-  integrations: [
-    tailwind(),
-    swup({
-      theme: false,
-      containers: ["#main-content"],
-      accessibility: false,
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind(), svelte()],
   build: {
     format: "file",
   },
