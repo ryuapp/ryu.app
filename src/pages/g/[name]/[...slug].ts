d5eb3dd86d5b1ce859e0a3c89c2de57d825cde33
ryu <114303361+ryuapp@users.noreply.github.com>
2023-12-22 23:08:47 +0900

    🦕 Create endpoints for Deno modules

---
+++ src/pages/g/[name]/[...slug].ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro"
+export const GET: APIRoute = ({ params, redirect }) => {
+  const name = params.name as string
+  const repo = name.split("@")[0]
+  const branch = name.split("@")[1] ?? "main"
+  const slug = (params.slug as string) ?? null
+
+  if (!slug) return redirect("/")
+
+  return redirect(`https://raw.githubusercontent.com/ryuapp/${repo}/${branch}/${slug}`, 301)
+}
