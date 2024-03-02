4249f49b8e1b0676422986ef95dc11a5c18b462d
ryu <114303361+ryuapp@users.noreply.github.com>
2024-03-02 14:50:37 +0900

    🔧 Integrate site config

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  title: "ryu.app",
+  mainNav: [
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "Commits",
+      href: "/commits",
+    },
+    {
+      title: "Privacy Policy",
+      href: "/privacy",
+    },
+  ],
+  links: {
+    github: "https://github.com/ryuapp/",
+  },
+}
--- src/config/site.ts
+++
@@ -1,3 +0,0 @@
-export const siteConfig = {
-  title: "ryu.app",
-}
