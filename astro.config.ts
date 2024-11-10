a0672057eda78ed36091767c72b31170e5b495df
ryu <114303361+ryuapp@users.noreply.github.com>
2024-11-10 20:36:15 +0900

    ⚡️ Optimize images

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ryu.app",
   output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   build: {
     format: "file",
   },
