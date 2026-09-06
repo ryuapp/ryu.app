b6a519918af7dd8375d630605ba87cda5e1b9118
Ryu <114303361+ryuapp@users.noreply.github.com>
2026-09-07 01:48:37 +0900

    ✨ Add js-semver

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -6,6 +6,11 @@ type Project = {
 
 export const projects = [
   {
+    name: "js-semver",
+    description: "A node-semver compliant Rust crate.",
+    link: "https://github.com/ryuapp/js-semver",
+  },
+  {
     name: "pindeps",
     description: "A CLI tool to pin dependency versions in JavaScript.",
     link: "https://github.com/ryuapp/pindeps",
@@ -14,11 +19,6 @@ export const projects = [
     name: "rb",
     description: "A rm(1) clone with the recycle bin for Windows.",
     link: "https://github.com/ryuapp/rb",
-  },
-  {
-    name: "Hono Hacker News",
-    description: "A Hacker News clone made with Hono and mono-jsx.",
-    link: "https://github.com/ryuapp/hono-hacker",
   },
   {
     name: "Fluent Emoji Picker",
